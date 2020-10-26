c8604b9abd1f457379a9c27878c6806831f6a6a2
ashen7 <986300260@qq.com>
2020-10-26 16:16:01 +0800

    更新

--- pytorch2onnx.py
+++ pytorch2onnx.py
@@ -6,24 +6,22 @@ from utils import *
 
 def main():
     args = parse_args()
-    dataset = args.dataset
+    dataset = "imagenet"
     model_name = args.model_name
     use_pretrain_model = args.use_pretrain_model
     use_multilabel = args.use_multilabel
     test_mode = args.test_mode
+
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    device_id = args.device_id
 
     image_size = args.image_size
     resize_h = args.resize_h
     resize_w = args.resize_w
     batch_size = args.batch_size
     model_path = os.getcwd() + "/models/{}_{}.pth".format(model_name, dataset)
-
-    if dataset == "cifar10":
-        image_size = 32
-    input_size = int(512 * (4 + (image_size / 32) - 1) * (4 + (image_size / 32) - 1))
-
-    # 构建网络
     num_classes = 1000
+    # 构建网络
     model = MobileNetV3_Small(num_classes)
     
     if use_multilabel:
@@ -54,7 +52,7 @@ def main():
 
     # 输入尺寸
     input_size = torch.randn(batch_size, 3, image_size, image_size, requires_grad=True)
-    output_onnx = model_name[:-4] + '.onnx'
+    output_onnx = 'onnx/' + model_name + '.onnx'
     torch.onnx.export(model, 
                       input_size, 
                       output_onnx, 
@@ -62,6 +60,7 @@ def main():
                       training=False, 
                       input_names=['input'], 
                       output_names=['output'])
+    print('Successfully export ONNX Model: {}'.format(output_onnx))
     
 if __name__ == '__main__':
     main()
